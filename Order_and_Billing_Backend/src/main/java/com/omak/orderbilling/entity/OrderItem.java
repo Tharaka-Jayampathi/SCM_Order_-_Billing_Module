4c00f8cf45887fd18d099975688a900cfa4d7a8f
Yuhansi07 <yuhansiyuthmi@gmail.com>
2026-06-08 08:13:02 +0530

    Add member5 order billing files

---
+++ Order_and_Billing_Backend/src/main/java/com/omak/orderbilling/entity/OrderItem.java
@@ -0,0 +1,37 @@
+package com.omak.orderbilling.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.ToString;
+import lombok.EqualsAndHashCode;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import java.math.BigDecimal;
+
+@Data
+@Entity
+@Table(name = "order_items")
+public class OrderItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "order_id", nullable = false)
+    @JsonBackReference
+    @ToString.Exclude
+    @EqualsAndHashCode.Exclude
+    private Order order;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "product_id", nullable = false)
+    private Product product;
+
+    @Column(nullable = false)
+    private Integer quantity;
+
+    @Column(nullable = false)
+    private BigDecimal unitPrice;
+
+    @Column(nullable = false)
+    private BigDecimal subtotal;
+}
---
+++ Order_and_Billing_Backend/src/main/java/com/omak/orderbilling/repository/OrderItemRepository.java
@@ -0,0 +1,9 @@
+package com.omak.orderbilling.repository;
+
+import com.omak.orderbilling.entity.OrderItem;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
+}
